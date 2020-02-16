1b3c7fca73dd505de687b458ad1963581ab52630
velveet <velveet@pm.me>
2020-02-16 09:10:38 -0500

    literal notation method for writing  objects

--- objects/objects.js
+++ objects/objects.js
@@ -1,10 +1,11 @@
-let person = new Object();
-
-person.firstname = 'John';
-person.lastname = 'Smith';
-
-person.son = new Object();
-person.son.name = 'Nick';
-person.son.age = 5;
+let person = {
+  firstname: 'John',
+  lastname: 'Smith',
+  age: 27,
+  daugther: {
+    name: 'Mary',
+    age: 5
+  }
+};
 
 console.log(person);
