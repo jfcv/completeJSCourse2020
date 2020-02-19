af610c89bf30889cbd6a2560005a553d329c9091
velveet <velveet@pm.me>
2020-02-19 10:30:44 -0500

    using .forEach() after converting the HTML Collection into Array using .forEach()

--- DOM/script.js
+++ DOM/script.js
@@ -1,15 +1,6 @@
 let icons = document.getElementsByClassName('fa');
-
-for (var i = 0; i < icons.length; i++) {
-  console.log(icons[i]);
-}
-
 let iconsArray = Array.from(icons);
 
-console.log(iconsArray);
-
-console.log(icons);
-
-iconsArray.push('Hello');
-
-console.log(iconsArray);
+iconsArray.forEach((icon, i) => {
+  console.log(icon, i);
+});
