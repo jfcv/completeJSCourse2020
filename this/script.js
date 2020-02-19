2ae132679a7670179091f25aaa0678544cc4053d
velveet <velveet@pm.me>
2020-02-19 08:56:07 -0500

    'this' as a Global Window Object

---
+++ this/script.js
@@ -0,0 +1,8 @@
+//this keyword usually refers to the Global Window Object
+//  this === window  ->   on the web browser
+
+function a() {
+  console.log(this); //this here refers to global window object
+};
+
+a();
