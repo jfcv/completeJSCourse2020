3b53352e504a6cdf0881fac0dc9f5bf9f073e65f
velveet <velveet@pm.me>
2020-02-20 09:30:13 -0500

    addEventListener() for listening to clicks on h2 header

--- DOM/script.js
+++ DOM/script.js
@@ -1,9 +1,5 @@
 let h2 = document.querySelector('header h2');
 
-h2.onclick = function() {
-  console.log('h2 clicked');
-}
-
-h2.onmouseover = function() {
-  console.log('h2 Mouse Over');
-}
+h2.addEventListener('click', function() {
+  console.log('clicked');
+});
