646255d09f0409602d818cb0d813d22ece2f8d89
velveet <velveet@pm.me>
2020-02-20 09:02:45 -0500

    Events, .onclick()

--- DOM/script.js
+++ DOM/script.js
@@ -1,11 +1,5 @@
 let h2 = document.querySelector('header h2');
 
-h2.className = 'changeBg';
-
-h2.classList.add('changeFt');
-
-//h2.classList.remove('changeBg');
-
-h2.classList.toggle('changeBg'); // adds or removes the class, depending on if it was active or not
-
-console.log(h2.classList);
+h2.onclick = function() {
+  console.log('h2 clicked');
+}
