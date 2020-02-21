3c2973108581b2926f6b3e4c01683c93eba4216b
velveet <velveet@pm.me>
2020-02-21 08:16:06 -0500

    sequence execution for events on HTML element that depends upon each other -> [div. header, h2]

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,17 @@
+let div = document.querySelector('div.wrapper');
+let header = document.querySelector('header');
 let h2 = document.querySelector('header h2');
 
-h2.addEventListener('click', function() {
-  console.log(this);
+div.addEventListener('click', function() {
+  console.log('From div');
 });
+
+header.addEventListener('click', function() {
+  console.log('From header');
+});
+
+h2.addEventListener('click', function() {
+  console.log('From h2');
+});
+
+//this code probes the sequences in which the different events executes
