9f74d8b892a58b1953b020b52b101654bd8e0cdc
velveet <velveet@pm.me>
2020-02-21 09:22:45 -0500

    coding task 3

---
+++ coding task 3/script.js
@@ -0,0 +1,54 @@
+//defining elements needed
+let btn = document.getElementById('add-btn');
+let input = document.getElementById('add-input');
+let list = document.getElementById('list');
+let form = document.getElementById('add');
+
+btn.addEventListener('click', function(e){
+
+    //prevents the page to reload, and probably other pre-settings
+    e.preventDefault();
+
+    //printing the clicks on the console
+    console.log('button "Add" clicked ');
+
+    //appends the new item to the list only if the form is not empty
+    if (input.value !== '') {
+      //create a new item on the list
+      let newItem = document.createElement('li');
+
+      //p element 1
+      let pElement1 = document.createElement('p');
+      let text = document.createTextNode(input.value);
+      pElement1.appendChild(text);
+
+      //p element 2
+      let pElement2 = document.createElement('p');
+
+        // i element 1
+        let iElement1 = document.createElement('i');
+        iElement1.setAttribute('class', 'fa fa-pencil-square-o');
+        pElement2.appendChild(iElement1);
+
+        // i element 2
+        let iElement2 = document.createElement('i');
+        iElement2.setAttribute('class', 'fa fa-times');
+        pElement2.appendChild(iElement2);
+
+      //input element
+      let inputElement = document.createElement('input');
+      inputElement.setAttribute('class', 'edit-note');
+      inputElement.setAttribute('type', 'text');
+
+      //appends HTML elements to the item
+      newItem.appendChild(pElement1);
+      newItem.appendChild(pElement2);
+      newItem.appendChild(inputElement);
+
+      //testing ..
+      console.log(newItem);
+
+      //appends the new item to the list
+      list.appendChild(newItem);
+    }
+});
