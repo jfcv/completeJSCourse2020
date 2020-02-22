2ca600ac6d2d7f54730ba5616413b9f45eafcb00
velveet <velveet@pm.me>
2020-02-22 09:55:36 -0500

    editing and deleting notes

--- coding task 3/script.js
+++ coding task 3/script.js
@@ -3,7 +3,10 @@ let btn = document.getElementById('add-btn');
 let input = document.getElementById('add-input');
 let list = document.getElementById('list');
 let form = document.getElementById('add');
+let ul = document.querySelector('ul');
 
+
+//adding items to the list
 btn.addEventListener('click', function(e){
 
     //prevents the page to reload, and probably other pre-settings
@@ -51,4 +54,51 @@ btn.addEventListener('click', function(e){
       //appends the new item to the list
       list.appendChild(newItem);
     }
+});
+
+
+//editing and deleting items
+ul.addEventListener('click', function(e){
+
+  if (e.target.classList[1] == 'fa-pencil-square-o') {
+
+    //hiding the parent node
+    let parentPar = e.target.parentNode;
+    parentPar.style.display = 'none';
+
+    //selecting siblings
+    let note = parentPar.previousElementSibling;
+    let input = parentPar.nextElementSibling;
+
+    //displaying the input
+    input.style.display = 'block';
+    input.value = note.textContent;
+
+    //pointers to the ul & li's elements
+    liItem = input.parentNode;
+    ulItem = input.parentNode.parentNode;
+
+    //editing event
+    input.addEventListener('keypress', function(e){
+      if (e.keyCode === 13) {
+        if (input.value !== '') {
+          note.textContent = input.value;
+          parentPar.style.display = 'block';
+          input.style.display = 'none';
+        } else {
+          ulItem.removeChild(liItem);
+        }
+      }
+    });
+
+  } else if (e.target.classList[1] == 'fa-times') {
+
+    //pointers to the ul & li items
+    ulItem = e.target.parentNode.parentNode.parentNode;
+    liItem = e.target.parentNode.parentNode;
+
+    //removing the element
+    ulItem.removeChild(liItem);
+  }
+
 });
