9379c54a152001bc3efc103640346723ea7e1050
velveet <velveet@pm.me>
2020-02-23 10:19:35 -0500

    function constructor

---
+++ function constructor/script.js
@@ -0,0 +1,10 @@
+function Person(firstname, lastname) {
+  this.firstname = firstname;
+  this.lastname = lastname;
+}
+
+let person1 = new Person('Joe', 'Ryan');
+let person2 = new Person('Mike', 'Ross');
+
+console.log(person1);
+console.log(person2);
