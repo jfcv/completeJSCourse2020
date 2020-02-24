b96e27a601744fdb61930dcea1699af35de1a05e
velveet <velveet@pm.me>
2020-02-24 15:37:03 -0500

    saving and object and a function on the function constructor prototype

--- function constructor and prototypes/script.js
+++ function constructor and prototypes/script.js
@@ -3,8 +3,14 @@ function Person(firstname, lastname) {
   this.lastname = lastname;
 }
 
+Person.prototype.getFullName = function() {
+  return this.firstname  + ' ' + this.lastname;
+}
+
+Person.prototype.greet = 'Hello from prototype object';
+
 let person1 = new Person('John', 'Smith');
 let person2 = new Person('Vick', 'Doe');
 
-console.log(person1);
-console.log(person2);
+console.log(person1.getFullName());
+console.log(person2.getFullName());
