df86a4621ea6a5fc078f54860e5fd59ad4b3e654
velveet <velveet@pm.me>
2020-02-25 09:23:40 -0500

    assigning a function() to a variable, wrapping it around will make appear the result on the console

--- IIFE/script.js
+++ IIFE/script.js
@@ -1,3 +1,10 @@
 (function(name) {
   console.log('Hello ' + name);
 })('John');
+
+let a  = function() {
+  console.log('Hi');
+  return 'Hello'; //the variable a takes the value of the returning value, despite what the function does
+}();
+
+console.log(a);
