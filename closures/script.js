a6bc1e81929031618cd46765c4b20e84fecf0f40
velveet <velveet@pm.me>
2020-02-25 09:41:55 -0500

    closures II

--- closures/script.js
+++ closures/script.js
@@ -1,13 +1,14 @@
-function calc(num1) {
-  let num2 = 10;
+function a() {
 
-  return function(num3) {
-    let sum = num1 + num2 + num3;
-    console.log(sum);
+  let arr = [];
+
+  for (var i = 0; i < 3; i++) {
+    arr.push((function() {
+      console.log(i);
+    })()); //IIFE that's why it could access to the counter's value : i
   }
+
+  return arr;
 }
 
-calc(5)(15); //2nd number inner variable 'num3'
-
-/*closures are about the inner functions accessing to the outer variables
-like the inner function(num3) accessing to num1 & num2 to calculate the result*/
+a();
