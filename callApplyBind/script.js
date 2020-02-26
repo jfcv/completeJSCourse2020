ab3838c477b6fe266d3f37375dce869125d18041
velveet <velveet@pm.me>
2020-02-26 08:47:14 -0500

    using call for modifying the properties inside an object

--- callApplyBind/script.js
+++ callApplyBind/script.js
@@ -4,11 +4,11 @@ var person = {
   fullname: function(age, job) {
     return this.firstname + ' ' + this.lastname;
   }
-}
+};
 
 let definePerson = function(age, job) {
   console.log(this.fullname() + ' is ' + age + ' years old and is a ' + job);
-}
+};
 
 definePerson.call(person, 28, 'Developer');
 
@@ -16,3 +16,11 @@ definePerson.apply(person, [30, 'Designer']);
 
 let getPerson = definePerson.bind(person, 35, 'musician');
 getPerson(); //invoking the function
+
+//using call for modifying the properties inside an object
+let nick = {
+  firstname: 'Nick',
+  lastname: 'Doe'
+};
+
+console.log(person.fullname.call(nick));
