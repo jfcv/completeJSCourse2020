cd01be37e04fcfcf0afb634625484ebd8a4c7ef3
velveet <velveet@pm.me>
2020-02-26 10:13:57 -0500

    Question() constructor created [quizController]. DOM elements selected [uiController]. selecting DOM elements using the [uiController] then using those for adding an event listener to the 'insert button' and when that button gets clicked it calls the addQuestionOnLocalStorage() method in [quizController]. All of that happened in the [controller]

--- project/script.js
+++ project/script.js
@@ -3,21 +3,59 @@
 //          QUIZ CONTROLLER
 let quizController = (function() {
 
-  //saving and recovering data from the local storage in the browser
-  localStorage.setItem('data',JSON.stringify([1, 2, 3, 4]));
-  console.log(JSON.parse(localStorage.getItem('data')));
+  //QUESTION CONSTRUCTOR
+  function Question(id, questionText, options, correctAnswer) {
+    this.id = id;
+    this.questionText = questionText;
+    this.options = options;
+    this.correctAnswer = correctAnswer;
+  }
 
+  return {
+    addQuestionOnLocalStorage: function(newQuestText, opts) {
+
+      let optionsArr = [];
+
+      for (var i = 0; i < opts.length; i++) {
+        if (opts[i].value !== "") {
+          optionsArr.push(opts[i].value);
+        }
+      }
+      console.log(optionsArr);
+    }
+  };
 })();
+
+
 
 //          UI CONTROLLER
 let uiController = (function() {
 
+  let domItems = {
+    //ADMIN PANEL ELEMENTS
+    questInsertBtn: document.getElementById('question-insert-btn'),
+    newQuestionText: document.getElementById('new-question-text'),
+    adminOptions: document.querySelectorAll('.admin-option')
+  };
+
+  //it must be return to make public and accesible from other methods
+  return {
+    getDomItems: domItems
+  };
+
 })();
 
 
-//          CONTROLLER
 
+//          CONTROLLER
 //controller is the interface between
 let controller = (function(quizCtrl, uiCtrl) {
+
+  let selectedDomItems = uiCtrl.getDomItems;
+
+  selectedDomItems.questInsertBtn.addEventListener('click', function() {
+    quizCtrl.addQuestionOnLocalStorage(selectedDomItems.newQuestionText, selectedDomItems.adminOptions);
+    
+  });
 
 })(quizController, uiController);
