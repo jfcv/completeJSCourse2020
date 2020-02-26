097e39de1bb185fc94d1942bbec2e0b910f6631f
velveet <velveet@pm.me>
2020-02-26 08:38:09 -0500

    using call() for invoking the 'this' Local Context Object from an object 'person' in another object 'definePerson'

---
+++ callApplyBind/script.js
@@ -0,0 +1,13 @@
+var person = {
+  firstname: 'John',
+  lastname: 'Smith',
+  fullname: function(age, job) {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+
+let definePerson = function(age, job) {
+  console.log(this.fullname() + ' is ' + age + ' years old and is a ' + job);
+}
+
+definePerson.call(person, 28, 'Developer');
