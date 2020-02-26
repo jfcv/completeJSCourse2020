cbdf538bf502b6377c8683f938612ff29bda58b2
velveet <velveet@pm.me>
2020-02-26 09:34:34 -0500

    saving and recovering data from the local storage in the browser

--- project/script.js
+++ project/script.js
@@ -1,27 +1,23 @@
 //module pattern used for handling this library
 
+//          QUIZ CONTROLLER
 let quizController = (function() {
-  let private = 10;
-  let privateFn = function(a) {
-    return a + private;
-  };
-  return {
-    publicMethod: function() {
-        return privateFn(20);
-    }
-  };
+
+  //saving and recovering data from the local storage in the browser
+  localStorage.setItem('data',JSON.stringify([1, 2, 3, 4]));
+  console.log(JSON.parse(localStorage.getItem('data')));
+
 })();
 
+//          UI CONTROLLER
 let uiController = (function() {
-  let num1 = 30;
-  return {
-    sum: function(num2) {
-      return num1 + num2;
-    }
-  };
+
 })();
+
+
+//          CONTROLLER
 
 //controller is the interface between
 let controller = (function(quizCtrl, uiCtrl) {
-  console.log(uiCtrl.sum(100) + quizCtrl.publicMethod());
+
 })(quizController, uiController);
